dbec8f7ff7e3fbcecd514e065b4fcce6abd32a5b
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2021-05-03 18:12:59 -0300

    Alteração da migration Categories
    Inseri a string name na migration categories.

--- pi3/database/migrations/2021_05_03_200724_create_categories_table.php
+++ pi3/database/migrations/2021_05_03_200724_create_categories_table.php
@@ -14,8 +14,8 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->name();
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
