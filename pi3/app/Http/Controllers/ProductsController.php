acf61c201de65c18d9b18af5f9ec3fe3673d7288
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2021-05-03 12:55:06 -0300

    Adição das funções na ProductController
    Adicionei as funções create, edit, update, destroy na product.

--- pi3/app/Http/Controllers/ProductsController.php
+++ pi3/app/Http/Controllers/ProductsController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductsController extends Controller
 {
-    //
-}
+    public function index(){
+        return view('product.index')->with('products', Product::all());
+    }
+    public function create(){
+        return view('product.create');
+    }
+    public function store(Request $request) {
+        Product::create($request->all());
+        session()->flash('success', 'Produto foi cadastrado com sucesso');
+        return redirect(route('product.index'));
+  //insert into products ('name','description','price') values ($request->name, $request->description, $request->price);
+        }
+
+        public function edit(Product $product){
+         return view('product.edit')->with('product',$product);
+        }
+        public function update(Request $request, Product $product){
+            $product->update($request->all());
+            session()->flash('success','Produto foi alterado com sucesso');
+            return redirect(route('product.index'));
+        }
+        public function destroy(Product $product){
+            $product->delete();
+            session()->flash('success','Produto foi apagado com sucesso');
+            return redirect(route('product.index'));
+        }
+        }
