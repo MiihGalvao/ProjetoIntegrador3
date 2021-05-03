ddd131f9a4b1fb7a3e536ac77ad4adf79da6a576
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2021-05-03 17:25:47 -0300

    Criação da migration de Products
    Criação da migration products e inserção dos atributos name, description, price na tabela.

--- pi3/database/migrations/2021_05_01_005818_create_products_table.php
+++ pi3/database/migrations/2021_05_01_005818_create_products_table.php
@@ -15,10 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name');   
             $table->string('description');
             $table->decimal('price', 9,2);
-            $table->timestamps();
+            $table->timestamps(); 
         });
     }
 
