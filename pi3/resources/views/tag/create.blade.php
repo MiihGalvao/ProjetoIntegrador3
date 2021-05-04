bb8613a8d202fe6228db3c560d9bec07fc92a69d
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2021-05-04 20:58:27 -0300

    Inserção da create tag
    Inseri a create.tag com todos os dados - Cadastra tag ,name e salvar.

---
+++ pi3/resources/views/tag/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   
+<title>Cadastra Tag</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body class= "container mt-5 bg-light">
+    <h1>Cadastra Tag</h1>
+    <form method="POST" action="{{ route('tag.store') }}" > 
+    @csrf
+<div class="row">
+    <span class = "form-label">Nome</span>
+    <input type="text" name="name"class="form-control">
+</div>
+<div class="row mt-4">
+    <button type="submit" class="btn btn-success btn-lg"> Salvar</button>
+</div>
+    </form>
+</body>
+</html>
