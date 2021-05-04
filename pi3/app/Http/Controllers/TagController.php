4b71d2bb4745ee5a4679232c9ba2b83ae4da5497
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2021-05-04 20:13:28 -0300

    Inserção dos dados na tag  controller
    Inseri as funções na tag  controller , index, create, store , edit, update.

--- pi3/app/Http/Controllers/TagController.php
+++ pi3/app/Http/Controllers/TagController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
+   /**
+ * Display  listing of the resource.
+ * 
+ * @return \Illuminate\Http\Response
+ */
+public function index()
+    {
+    return view('tag.index')->with('tags', Tag::all());
+    }
+
+public function create()
+    {
+    return view('tag.create');
+    }
+
+public function store(Request $request)
+    {
+    Tag::create($request->all());
+    session()->flash('success', 'Tag foi cadastrada com sucesso');
+    return redirect(route('tag.index'));
+    }
+
+
+public function show(Tag $tag)
+    {
     //
+    }
+
+public function edit(Tag $tag)
+    {
+    return view('tag.edit')->with('tag', $tag);
+    }
+
+public function update(Request $request, Tag $tag)
+    {
+    $tag->update($request->all());
+    session()->flash('success','Tag foi alterada com sucesso');
+    return redirect(route('tag.index'));
+    }
+
+
+public function destroy(Tag $tag)
+    {
+    $tag->delete();
+    session()->flash('success','Tag foi apagada com sucesso');
+    return redirect(route('tag.index'));
+    }
+
 }
