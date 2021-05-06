4556c5f832963089c78ef672c940aed29ad744a1
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2021-05-05 23:45:04 -0300

    Inserção das rotas corretas no web.php
    Inseri as rotas corretas product, tag e category.

--- pi3/routes/web.php
+++ pi3/routes/web.php
@@ -15,29 +15,61 @@ use App\Http\Controllers\TagController;
 |
 */
 
+require __DIR__.'/auth.php';
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Product
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+//Route::get('/semlogin', function () {
+  //  return view('deslogado');
+//})->middleware(['guest']);
+
+
+Route::resource('/product', ProductsController::class);
+//Route::get('/trash/product', [ProductsController::class, 'trash'])->name('product.trash');
+//Route::patch('/trash/product/{id}', [ProductsController::class, 'restore'])->name('product.restore');
+
+Route::resource('/category', CategoriesController::class);
+Route::resource('/tag', TagController::class);
+
+
+
+
+
+
+
+
+
+
+/*Product
 //Route::resource('/product', ProductsController::class); tem a msm função
 //que o código abaixo..
-Route::get('/product', [ProductsController::class, 'index'])->name('product.index'); 
+/*Route::get('/product', [ProductsController::class, 'index'])->name('product.index'); 
 Route::get('/product/create',[ProductsController::class,'create'])->name('product.create');
 Route::post('/product/store', [ProductsController::class,'store'])->name('product.store');
 Route::get('/product/edit/{product}', [ProductsController::class,'edit'])->name('product.edit');
 Route::post('/product/update/{product}',[ProductsController::class,'update'])->name('product.update');
 Route::get('/product/destroy/{product}',[ProductsController::class,'destroy'])->name('product.destroy');
 
+
 //Categories
 //Route::resource('/category', CategoriesController::class); tem a msm função
 //que o código abaixo..
-Route::get('/category', [CategoriesController::class, 'index'])->name('category.index'); 
+/*Route::get('/category', [CategoriesController::class, 'index'])->name('category.index'); 
 Route::get('/category/create',[CategoriesController::class,'create'])->name('category.create');
 Route::post('/category/store', [CategoriesController::class,'store'])->name('category.store');
 Route::get('/category/edit/{category}', [CategoriesController::class,'edit'])->name('category.edit');
 Route::post('/category/update/{category}',[CategoriesController::class,'update'])->name('category.update');
 Route::get('/category/destroy/{category}',[CategoriesController::class,'destroy'])->name('category.destroy');
 
-//Tags
+
+Tags
 Route::resource('/tag', TagController::class);
+*/
