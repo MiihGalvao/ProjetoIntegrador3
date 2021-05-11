c72453e5856e28d4d064e77120b7e98ccb79b076
Milena Galvao <milenasg91@gmail.com>
2021-05-10 22:28:11 -0300

    alterei o nome softdeletes

--- pi3/database/migrations/2021_05_10_180718_add_softdelete_product.php
+++ pi3/database/migrations/2021_05_10_180718_add_softdelete_product.php
@@ -14,7 +14,7 @@ class AddSoftdeleteProduct extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->softDeletes();
+            $table->SoftDeletes();
         });
     }
 
