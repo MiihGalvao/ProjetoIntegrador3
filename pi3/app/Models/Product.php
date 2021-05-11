e611f679ea8537c20a335d60769812bb3e439c90
Milena Galvao <milenasg91@gmail.com>
2021-05-10 22:11:42 -0300

    adicionei o caminho softdelete na product
    adicionei o softdelete na product

--- pi3/app/Models/Product.php
+++ pi3/app/Models/Product.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquen\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory;
+    use softDeletes;
+    
     protected $fillable = ['name','description','price','category_id'];
 
     public function category(){
