b3f2c9821ea7de2626b4bfe95bf8cfb0785ba7fa
Milena Galvao <milenasg91@gmail.com>
2021-05-12 22:00:54 -0300

    inserção da função tag dentro da product
    inserção da função tag dentro da product - belongsToMany.

--- pi3/app/Models/Product.php
+++ pi3/app/Models/Product.php
@@ -16,4 +16,7 @@ class Product extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
