8baa330b93a2d5f7ca97c2df1b81808b3b2a320b
Milena Galvao <milenasg91@gmail.com>
2021-05-12 22:01:35 -0300

    criei a migration add_relation_product_and_tag
    criei a migration add_relation_product_and_tag

---
+++ pi3/database/migrations/2021_05_13_004705_add_relation_product_and_tag.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRelationProductAndTag extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_tag', function (Blueprint $table) {
+           $table->integer('product_id');
+           $table->integer('tag_id');
+          
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('product_tag');
+    }
+}
