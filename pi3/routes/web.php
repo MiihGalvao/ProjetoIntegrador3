eee6165e0464108bdefd1eb347440abbacb6fd6b
Milena Galvao <milenasg91@gmail.com>
2021-05-12 21:29:01 -0300

    Descomentei a linha product id
    Descomentei a linha product id

--- pi3/routes/web.php
+++ pi3/routes/web.php
@@ -34,9 +34,9 @@ Route::get('/dashboard', function () {
 
 Route::resource('/product', ProductsController::class);
 Route::get('/trash/product', [ProductsController::class, 'trash'])->name('product.trash');
-//Route::patch('/trash/product/{id}', [ProductsController::class, 'restore'])->name('product.restore');
+Route::patch('/trash/product/{id}', [ProductsController::class, 'restore'])->name('product.restore');
 Route::patch('/trash/product', [ProductsController::class, 'trash'])->name('product.trash');
-Route::patch('/product/restore/{Product}', [ProductsController::class, 'restore'])->name('product.restore');
+//Route::patch('/product/restore/{Product}', [ProductsController::class, 'restore'])->name('product.restore');
 
 
 Route::resource('/category', CategoriesController::class);
