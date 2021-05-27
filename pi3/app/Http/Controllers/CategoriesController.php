691f50dbd674c7f621ef44fc895f392ffed49b28
Milena Galvao <milenasg91@gmail.com>
2021-05-26 23:38:17 -0300

    acrescentei () depois de products
    acrescentei () depois de products

--- pi3/app/Http/Controllers/CategoriesController.php
+++ pi3/app/Http/Controllers/CategoriesController.php
@@ -16,7 +16,7 @@ class CategoriesController extends Controller
 }
 
     public function show(Category $category){
-        return view('category.show')->with(['category' => $category), 'products' => $category->products->paginate(3)];
+        return view('category.show')->with(['category' => $category, 'products' => $category->products()->paginate(3)];
     }
 
 
