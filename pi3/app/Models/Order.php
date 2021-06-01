6378e3e9d685eb10a5326f742a8dbe0e672146bc
Milena Galvao <milenasg91@gmail.com>
2021-05-31 23:57:32 -0300

    Adicionei os atributos protegidos na model de order
    Adicionei os atributos protegidos na model de order -['user_id', 'status', 'address', 'address_number', 'address_city', 'addres_state','cc_number'];

--- pi3/app/Models/Order.php
+++ pi3/app/Models/Order.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'status', 'address', 'address_number', 'address_city', 'addres_state','cc_number'];
 }
