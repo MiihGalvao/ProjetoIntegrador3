526f7b83bd47a0e852b80d4b3fdbac8b291782b9
Milena Galvao <milenasg91@gmail.com>
2021-06-01 23:35:48 -0300

    acrescentei a function de productt nos pedidos
    acrescentei a function de productt nos pedidos

--- pi3/app/Models/OrderItem.php
+++ pi3/app/Models/OrderItem.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
-    protected $fillable = ['product_id', 'quantity', 'price'];
-}
+    protected $fillable = ['order_id','product_id', 'quantity', 'price'];
+
+    public function product(){
+        return Product::where('id', '=', $this->product_id)->firts();
+       
+    }
 }
