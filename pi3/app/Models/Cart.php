4818563209ce9e3bae4a934351cdc8268bd24740
Milena Galvao <milenasg91@gmail.com>
2021-06-02 23:27:02 -0300

    tirei o s de products
    tirei o s de products

--- pi3/app/Models/Cart.php
+++ pi3/app/Models/Cart.php
@@ -12,7 +12,7 @@ class Cart extends Model
     protected $fillable = ['user_id','product_id','quantity'];
 
     public function product(){
-        return products::where('id', '=', $this->products_id)->first();
+        return product::where('id', '=', $this->products_id)->first();
     }
 
     public static function count(){
